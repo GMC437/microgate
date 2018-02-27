fafb656a8b59b8519a005e4c7d23f9301552e494
Grant <grantmcnally.dev@gmail.com>
2018-02-27 20:02:47 +0000

    basic setup

---
+++ index.js
@@ -0,0 +1,20 @@
+'use strict';
+var http = require('http');
+var fs = require('fs');
+var path = require('path');
+
+var server = http.createServer(function (req, res) {
+    var url = req.url;
+    
+    var fileName = 'index.html'
+    if (url.length > 1) {
+        fileName = url.substring(1);
+    }
+    console.log(fileName);
+    var filePath = path.resolve(__dirname, 'app', fileName);
+    fs.readFile(filePath, function (err, data) {
+       res.end(data); 
+    });
+});
+
+server.listen(3000);
