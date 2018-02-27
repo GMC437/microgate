a0fac4610d052484bb838df4f7263a2a5bb84f75
Grant <grantmcnally.dev@gmail.com>
2018-02-27 22:31:57 +0000

    mock get users complete

--- index.js
+++ index.js
@@ -1,20 +1,19 @@
 'use strict';
-var http = require('http');
-var fs = require('fs');
-var path = require('path');
+var express = require('express');
+var app = express();
+var fs = require("fs");
 
-var server = http.createServer(function (req, res) {
-    var url = req.url;
-    
-    var fileName = 'index.html'
-    if (url.length > 1) {
-        fileName = url.substring(1);
-    }
-    console.log(fileName);
-    var filePath = path.resolve(__dirname, 'app', fileName);
-    fs.readFile(filePath, function (err, data) {
-       res.end(data); 
-    });
-});
+app.get('/listUsers', function (req, res) {
+   fs.readFile( __dirname + "/app/mockJSON/" + "users.json", 'utf8', function (err, data) {
+       res.end( data );
+   });
+})
 
-server.listen(3000);
+var server = app.listen(3005, function () {
+
+  var host = server.address().address
+  var port = server.address().port
+
+  console.log("Example app listening at http://%s:%s", host, port)
+
+})
